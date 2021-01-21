685260f83f36a73206fda38c79aa416cf351cfea
mondobizz <caseyjhatley@yahoo.com>
2021-01-21 09:19:30 -0500

    another commit

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,36 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
-              },
-        ]
-    }
-}
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: [
+                    "file-loader",
+                    {
+                            loader: "image-webpack",
+                            options: {
+                                gifsicle: {
+                                    interlanced: false
+                                },
+                            },
+                            optipng: {
+                                optimizationLevel: 7
+                            },
+                            pngquant: {
+                                quality: "65-90",
+                                speed: 4
+                            },
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
