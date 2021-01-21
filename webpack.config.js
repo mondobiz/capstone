481332917260af6d0958cdfec0801aa7687984b4
mondobizz <caseyjhatley@yahoo.com>
2021-01-21 17:19:22 -0500

    import font css error

--- webpack.config.js
+++ webpack.config.js
@@ -8,39 +8,6 @@ module.exports = {
                     loader: "babel-loader"
                 }
             },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                use: [
-                    "file-loader",
-                    {
-                            loader: "image-webpack",
-                            options: {
-                                gifsicle: {
-                                    interlanced: false
-                                },
-                            },
-                            optipng: {
-                                optimizationLevel: 7
-                            },
-                            pngquant: {
-                                quality: "65-90",
-                                speed: 4
-                            },
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65
-                            }
-                    }
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
         ],
     },
 };
